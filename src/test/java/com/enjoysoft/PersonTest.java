cd71b5ffc2fad90af8fa3c89cb112f6fb9c70042
Jerry Ying <enjoysoft@gmail.com>
2013-12-28 23:19:20 +0800

    inital commit

---
+++ src/test/java/com/enjoysoft/PersonTest.java
@@ -0,0 +1,13 @@
+package com.enjoysoft;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PersonTest {
+	@Test
+	public void canConstructAPersonWithAName() {
+		Person person = new Person("Larry");
+		assertEquals("Larry", person.getName());
+	}
+}
